7cfbfb54bb2cc3a01198f5e7ef300ce091df93cc
EBSGTG <gerty438@gmail.com>
2022-10-08 17:38:39 +0300

    just rofl

--- ConsoleApp1/Lab1/Program.cs
+++ ConsoleApp1/Lab1/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Game
+﻿namespace ConsoleApp1.Lab1
 {
-    internal class Program
+    internal class Lab1
     {
-        static void Main(string[] args)
+        
+
+         class GameAccount
         {
-         
+            
+             string UserName { get; set; }
+             int GamesCount { get; set; }
+             int CurrentRating { get; set; }
+
+
+            public GameAccount(string name, int games, int rating)
+            {
+                UserName = name;
+                GamesCount = games;
+                CurrentRating = rating;
+                
+            }
+            static void Main(string[] args)
+            {
+                GameAccount aw = new GameAccount("jopka", 0, 100);
+                Console.WriteLine(aw.UserName + " " + aw.GamesCount + " " + aw.CurrentRating);
+            }
+            
+            public class WinGame 
+            {
+                
+            }
+
+            public class LoseGame
+            {
+                
+            }
+
+            public class GetStats
+            {
+                
+            }
         }
     }
 }
