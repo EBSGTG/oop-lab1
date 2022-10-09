2547983245d42607667eae1d49d061b9f3d2bf88
EBSGTG <gerty438@gmail.com>
2022-10-09 15:11:04 +0300

    Final with 1 mistake

--- ConsoleApp1/Lab1/Program.cs
+++ ConsoleApp1/Lab1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Net.Sockets;
+using System.Xml;
 
 namespace ConsoleApp1.Lab1
 {
@@ -9,11 +10,16 @@ namespace ConsoleApp1.Lab1
     {
         static void Main(string[] args)
         {
-            var playerOne = new GameAccount("ANTON");
+            var playerOne = new GameAccount("Bender");
             var playerTwo = new GameAccount("STEPAN");
             var gameone = new Game(playerOne, playerTwo, 10); 
-            gameone.Start();
+            gameone.PowerCheck();
+            gameone.PowerCheck();
+            gameone.PowerCheck();
+            gameone.PowerCheck();
+            gameone.PowerCheck();
             Console.WriteLine(playerOne.GetStats());
+            Console.WriteLine(playerTwo.GetStats());
         }
 
         public class GameAccount
@@ -21,12 +27,12 @@ namespace ConsoleApp1.Lab1
             public string UserName { get; }
             public int GamesCount { get; }
 
-            public int GameId
+            public int GameIndex
             {
                 get
                 {
-                    int gameId = 0;
-                    foreach (var t in allRatingCalculations)
+                    int gameId = 1;
+                    foreach (var t in allOperations)
                     {
                         gameId += t.GameIndex;
                     }
@@ -36,52 +42,47 @@ namespace ConsoleApp1.Lab1
                 
             }
 
-            public int RatingPoints
+            public int CurrentRating
             {
                 get
                 {
-                    int ratingPoints = 100;
-                    foreach (var t in allRatingCalculations)
+                    int currentRating = 1000;
+                    foreach (var t in allOperations)
                     {
-                        ratingPoints = t.Rating;
+                        currentRating = t.Rating;
                     }
-                    return ratingPoints;
+                    return currentRating;
                 }
             }
-
-            public void GameStart(int rating, string status, int gameId)
-            {
-                var gameStart = new RatingCalculation(rating, status, "Start of game", GameId);
-                allRatingCalculations.Add(gameStart);
-            }
-
+            
             public void WinGame(string opponentName, int rating)
             {
-                var winGame = new RatingCalculation( rating, "Win", opponentName, 1);
-                allRatingCalculations.Add(winGame);
+                var winGame = new Operation( rating, "Win", opponentName, 1);
+                allOperations.Add(winGame);
             }
 
             public void LoseGame(string opponentName, int rating)
             {
-                if (RatingPoints - rating < 1)
+                if (CurrentRating - rating < 1)
                 {
                     throw new InvalidOperationException("Game rating cant be less than 1");
                 }
-                var loseGame = new RatingCalculation(rating, "Lose", opponentName, 1);
-                allRatingCalculations.Add(loseGame);
+                Console.WriteLine(CurrentRating-rating);
+                var loseGame = new Operation(-rating, "Lose", opponentName, 1);
+                allOperations.Add(loseGame);
             }
 
             public string GetStats()
             {
                 var rep = new System.Text.StringBuilder();
                 int gameId = 0;
-                int ratingPoints = 1000;
-                rep.AppendLine("|Player|\t\t|CurrentRating|\\t\t|Status|\t\t|OpponentName|\t\t|GameRating|\t\t|GameId|");
-                foreach (var t in allRatingCalculations)
+                int currentRating = 1000;
+                rep.AppendLine("|Player|\t|CurrentRating|\t\t|Status|\t|OpponentName|\t|GameRating|\t|GameId|");
+                foreach (var t in allOperations)
                 {
-                    ratingPoints += t.Rating;
+                    currentRating += t.Rating;
                     gameId += t.GameIndex;
-                    rep.AppendLine($"|{UserName}|\t\t|{ratingPoints}| \t\t|{t.Status}|\t\t|{t.OpponentName}|\t\t|{t.Rating}|\t\t|{GameId}|");
+                    rep.AppendLine($"|{UserName}|\t|{currentRating}|\t\t\t|{t.Status}|\t\t|{t.OpponentName}|\t|{t.Rating}|\t\t|{gameId}|");
                 }
 
                 return rep.ToString();
@@ -90,23 +91,23 @@ namespace ConsoleApp1.Lab1
             {
                 UserName = name;
                 GamesCount = 0;
-                GameStart(10,"Game",10);
             }
             
             
-            private List<RatingCalculation> allRatingCalculations = new List<RatingCalculation>();
+            
+            private List<Operation> allOperations = new List<Operation>();
            
             
         }
         
-        public class RatingCalculation
+        public class Operation
         {
             public int Rating { get; }
             public string Status { get; }
             public string OpponentName { get; }
             public int GameIndex { get; }
 
-            public RatingCalculation(int rating, string status, string opponentName, int gameIndex)
+            public Operation(int rating, string status, string opponentName, int gameIndex)
             {
                 Rating = rating;
                 Status = status;
@@ -119,10 +120,13 @@ namespace ConsoleApp1.Lab1
         {
             public readonly GameAccount PlayerOne;
             public readonly GameAccount PlayerTwo;
+            
+            
             public  int Rating {get;}
 
             public Game(GameAccount p1, GameAccount p2, int rating)
             {
+                
                 if (rating <= 0)
                 {
                     throw new ArgumentOutOfRangeException(nameof(rating),"incorrect rating");
@@ -134,16 +138,20 @@ namespace ConsoleApp1.Lab1
 
             }
 
-            public void Start()
+            public void PowerCheck()
             {
-                var random = new Random();
-                int oor = random.Next(0, 2);
-                if (oor != 0)
+                var r = new Random();
+                int power1 = r.Next(1, 999);
+                int power2 = r.Next(1, 999);
+                Console.WriteLine("\nGame #"+PlayerOne.GameIndex +" for "+ Rating + "pts");
+                if (power1 > power2)
                 {
                     PlayerOne.WinGame(PlayerTwo.UserName, Rating);
                     PlayerTwo.LoseGame(PlayerOne.UserName, Rating);
+                    Console.WriteLine(PlayerOne.UserName  +" "+ power1 +" power " + " wins " + PlayerTwo.UserName +" "+power2 +" power ");
                 }
                 else {
+                    Console.WriteLine(PlayerTwo.UserName  +" " +  power2 +" power " + " wins " + PlayerOne.UserName  +" "+ power1 +" power ");
                     PlayerTwo.WinGame(PlayerOne.UserName, Rating);
                     PlayerOne.LoseGame(PlayerTwo.UserName,Rating);
                 }
